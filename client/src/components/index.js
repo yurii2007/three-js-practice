cb67f8fc340015fa06dad3c6440d713d288d90cc
yurii2007 <yurash862@gmail.com>
2023-09-17 21:47:07 +0300

    configuration

---
+++ client/src/components/index.js
@@ -0,0 +1,6 @@
+import CustomButton from "./CustomButton";
+import FilePicker from "./FilePicker";
+import ColorPicker from "./ColorPicker";
+import Tab from "./Tab";
+
+export { CustomButton, FilePicker, ColorPicker, Tab };
